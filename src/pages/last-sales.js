9d6c6dddc9395d4d3aecb907b0f04b26a818bb64
Your Name <1821503@iub.edu.bd>
2025-04-10 12:34:23 +0600

    All

--- src/pages/last-sales.js
+++ src/pages/last-sales.js
@@ -1,35 +1,74 @@
-import { useEffect, useState } from "react";
+// pages/last-sales.js
 
-function LastSalesPage(){
-    const [sales,setSales]=useState();
-    const [isloading,setIsLoading]=useState(false);
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
-    useEffect(()=>{
-        setIsLoading(true);
-        fetch('https://data-fetching-7ebfc-default-rtdb.firebaseio.com/sales.json')
-        .then(response=>response.json())
-        .then(data=>{
-            const transformedSales=[];
-            for(const key in data){
-                transformedSales.push({id:key,username:data[key].username,volume: data[key].volume,
+// SWR fetcher
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
-                })
+function LastSalesPage(props) {
+    const [sales, setSales] = useState(props.sales);
+
+    const { data, error } = useSWR(
+        'https://data-fetching-7ebfc-default-rtdb.firebaseio.com/sales.json',
+        fetcher
+    );
+
+    useEffect(() => {
+        if (data) {
+            const transformedSales = [];
+
+            for (const key in data) {
+                transformedSales.push({
+                    id: key,
+                    username: data[key].username,
+                    volume: data[key].volume,
+                });
             }
+
             setSales(transformedSales);
-            setIsLoading(false);
+        }
+    }, [data]);
 
-        })
-    },[])
-
-    if(isloading){
-        return <p>Loading...</p>
+    if (error) {
+        return <p>NO data yet</p>;
     }
-    if(!sales){
-        return <p>NO data yet</p>
-    }
-    return <ul>
-        {sales.map(sale=><li key={sale.id}>{sale.username}-${sale.volume}</li>)}
-    </ul>
 
+    if (!sales) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <ul>
+            {sales.map((sale) => (
+                <li key={sale.id}>
+                    {sale.username} - ${sale.volume}
+                </li>
+            ))}
+        </ul>
+    );
 }
+
+export async function getStaticProps() {
+    const res = await fetch('https://data-fetching-7ebfc-default-rtdb.firebaseio.com/sales.json');
+    const data = await res.json();
+
+    const transformedSales = [];
+
+    for (const key in data) {
+        transformedSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume,
+        });
+    }
+
+    return {
+        props: {
+            sales: transformedSales,
+        },
+        revalidate: 10, // Re-generate page at most every 10 seconds
+    };
+}
+
 export default LastSalesPage;
